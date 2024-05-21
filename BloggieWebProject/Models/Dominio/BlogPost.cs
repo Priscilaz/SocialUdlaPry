4e72e10a980659ff2b03cbde6577ff2a4b1cb196
DanielaMoraDevJourney <danielamoradev@outlook.com>
2024-05-21 18:59:06 -0500

    Creación de las clases base en el modelo del proyecto "BlogPost" y "Tags".
    Instalación de los paquetes:
    Microsoft.EntityFrameworkCore.SqlServer
    Microsoft.EntityFrameworkCore.Tools

---
+++ BloggieWebProject/Models/Dominio/BlogPost.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BloggieWebProject.Models.Dominio
+{
+    public class BlogPost
+    {
+        public Guid Id { get; set; }
+
+        [Required]
+        public string? Encabezado { get; set; }
+
+        [Required]
+        public string? TituloPagina { get; set; }
+
+        [Required]
+        public string? Contenido { get; set; }
+
+        [Required]
+        public string? DescripcionCorta { get; set; }
+
+        public string? UrlImagenDestacada { get; set; }
+
+        public string? ManejadorUrl { get; set; }
+
+        [Required]
+        public DateTime FechaPublicacion { get; set; }
+
+        [Required]
+        public string? Autor { get; set; }
+
+        [Required]
+        public bool Visible { get; set; }
+
+        [Required]
+        public ICollection<Tag> Tags { get; set; }  //Un post tendrá una colección de tags
+    }
+}
---
+++ BloggieWebProject/Models/Dominio/Tag.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BloggieWebProject.Models.Dominio
+{
+    public class Tag
+    {
+        public Guid Id { get; set; }
+
+        [Required]
+        public string? Nombre { get; set; }
+
+        [Required]
+        public string? DisplayNombre { get; set; }
+
+        [Required]
+
+        public ICollection<BlogPost> BlogPosts { get; set; }  //Un tag tendrá multiples post
+    }
+}

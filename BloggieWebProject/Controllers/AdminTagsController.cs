2394bc276df1390ecfbec5b0b413d7770a1e36ae
DanielaMoraDevJourney <danielamoradev@outlook.com>
2024-05-21 20:07:57 -0500

    Creación del Admin Tag controller, su respectiva vista y punto de entrada desde el "Home".

---
+++ BloggieWebProject/Controllers/AdminTagsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BloggieWebProject.Controllers
+{
+    public class AdminTagsController : Controller
+    {
+        [HttpGet]
+        public IActionResult Agregar()
+        {
+            return View();
+        }
+    }
+}

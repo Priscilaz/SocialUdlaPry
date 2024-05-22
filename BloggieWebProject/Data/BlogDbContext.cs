07212f80cd2653bcdc8cf916fbf1a4609a72e84f
DanielaMoraDevJourney <danielamoradev@outlook.com>
2024-05-21 19:18:45 -0500

    Creación del DbContext y inyección del Db Context dentro de los servicios de la aplicación.

---
+++ BloggieWebProject/Data/BlogDbContext.cs
@@ -0,0 +1,18 @@
+﻿using BloggieWebProject.Models.Dominio;
+using Microsoft.EntityFrameworkCore;
+
+namespace BloggieWebProject.Data
+{
+    public class BlogDbContext : DbContext
+    {
+        public BlogDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<BlogPost> BlogPosts { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+
+
+    }
+}

fffda1dfadac49b5051f0524a146e6a1ad47a0a0
DanielaMoraDevJourney <danielamoradev@outlook.com>
2024-06-04 12:15:04 -0500

    Views y Controllers del Blog

---
+++ BloggieWebProject/Controllers/AdminBlogPostController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BloggieWebProject.Controllers
+{
+    public class AdminBlogPostController : Controller
+    {
+        public IActionResult AgregarPost()
+        {
+            return View;
+        }
+    }
+}

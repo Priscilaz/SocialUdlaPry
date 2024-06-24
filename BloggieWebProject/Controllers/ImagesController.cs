5c30ce55fff9396e0771495cc1bd9d3763c2d0b3
DanielaMoraDevJourney <danielamoradev@outlook.com>
2024-06-23 22:59:17 -0500

    Parte 1 API

---
+++ BloggieWebProject/Controllers/ImagesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BloggieWebProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImagesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Ok("GET Images API llamada");
+        }
+
+    }
+}
